adf167345bef62dd6301fc7c4eead79da472cf1e
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-09-16 22:04:40 +0300

    Taken screenshots

--- backend/src/api/tasks.js
+++ backend/src/api/tasks.js
@@ -1,43 +1,43 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-let tasks = [
+const tasks = [
   {
     id: 1,
-    name: "WALK THROUGH THE WAY OF KNOWLEDGE",
+    name: 'WALK THROUGH THE WAY OF KNOWLEDGE',
   },
   {
     id: 2,
-    name: "GROW WITH DEVOPS",
+    name: 'GROW WITH DEVOPS',
   },
   {
     id: 3,
-    name: "CODE EAT SLEEP CODE - REPEAT",
+    name: 'CODE EAT SLEEP CODE - REPEAT',
   },
 ];
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json(tasks);
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params; // destructuring
   const task = tasks.find((m) => m.id === Number(id));
   if (task) {
     res.status(200).json(task);
   } else {
-    res.status(404).json({ message: "Not found" });
+    res.status(404).json({ message: 'Not found' });
   }
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const { id, name } = req.body;
   tasks.push({ id, name });
-  res.status(201).json({ message: "Created" });
+  res.status(201).json({ message: 'Created' });
 });
 
 // update tasks
-router.patch("/:id", (req, res) => {
+router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
   const index = tasks.findIndex((m) => m.id === Number(id));
@@ -46,13 +46,14 @@ router.patch("/:id", (req, res) => {
     name,
   };
   tasks[index] = updatedTask;
-  res.status(200).json({ message: "Updated" });
+  res.status(200).json({ message: 'Updated' });
 });
 
-delete router.delete("/:id", (req, res) => {
+delete router.delete('/:id', (req, res) => {
   const { id } = req.params;
+  // eslint-disable-next-line no-const-assign
   tasks = tasks.filter((m) => m.id !== Number(id));
-  res.status(200).json({ message: "Deleted" });
+  res.status(200).json({ message: 'Deleted' });
 });
 
 module.exports = router;
